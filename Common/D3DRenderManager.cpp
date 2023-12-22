e4d0a46641827aece1d085787e37e9632a6b99e1
LeeDongWon <ldw9981@gmail.com>
2023-12-22 17:50:42 +0900

    임시 렌더링 코드 변경

--- Common/D3DRenderManager.cpp
+++ Common/D3DRenderManager.cpp
@@ -369,8 +369,10 @@ void D3DRenderManager::Render()
 			ApplyMaterial(meshInstance->m_pMaterial);	// 머터리얼 적용
 			pPrevMaterial = meshInstance->m_pMaterial;
 		}
-				
-		m_TransformW.mWorld = meshInstance->m_pNodeWorldTransform->Transpose();
+		
+		Math::Matrix rot = Math::Matrix::CreateFromYawPitchRoll(m_Rotation.y, m_Rotation.x, m_Rotation.z);
+
+		m_TransformW.mWorld = rot.Transpose();// meshInstance->m_pNodeWorldTransform->Transpose();
 		m_pDeviceContext->UpdateSubresource(m_pCBTransformW, 0, nullptr, &m_TransformW, 0, 0);
 		
 		// Draw
