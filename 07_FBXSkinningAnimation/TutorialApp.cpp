d896b704f97b82042cabcd85b75fc9c1c687cc7f
LeeDongWon <ldw9981@gmail.com>
2023-12-24 05:47:11 +0900

    코드 정리

--- 07_FBXSkinningAnimation/TutorialApp.cpp
+++ 07_FBXSkinningAnimation/TutorialApp.cpp
@@ -48,7 +48,7 @@ bool TutorialApp::Initialize(UINT Width, UINT Height)
 
 	StaticMeshModel& model = m_StaticMeshModelList.emplace_back();
 
-	auto SceneResource = ResourceManager::Instance->CreateStaticMeshSceneResource("../Resource/Vampire.fbx"); //cerberus
+	auto SceneResource = ResourceManager::Instance->CreateStaticMeshSceneResource("../Resource/Zombie.fbx"); //cerberus
 	model.SetSceneResource(SceneResource);
 	
 	return true;
